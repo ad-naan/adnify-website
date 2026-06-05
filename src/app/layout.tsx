7be58af0b5e75c673d3aec0721267b9f5c635731
adnaan <163977012+ad-naan@users.noreply.github.com>
2026-06-05 12:15:39 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,11 +46,11 @@ export default function RootLayout({
       <body className="antialiased bg-black text-white selection:bg-white/20">
         {children}
         <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2158925493674970"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+  async
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2158925493674970"
+  crossOrigin="anonymous"
+  strategy="afterInteractive"
+/>
       </body>
     </html>
   );
